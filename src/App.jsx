0efc4900ab0d08efbf05727adb4ca9ad33ff4728
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2025-03-13 20:15:19 +0530

    add operator page samples

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Operator/Dashboard"
+import Inventory from "./components/Operator/Inventory"
+import Report from "./components/Operator/Report"
 function App() {
   return (
     <>
-      <h1 className='text-3xl text-red-400'>Hello World </h1> 
+    <Router>
+      <Routes>
+        <Route path="/operator-dashboard" Component={Dashboard} />
+        <Route path="/operator-inventory" Component={Inventory} />
+        <Route path="/operator-reports" Component={Report} />
+      </Routes>
+    </Router>
     </>
   )
 }
